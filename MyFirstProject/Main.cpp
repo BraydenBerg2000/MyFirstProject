055fa5eb4c13142487db64bb3f3d045fe9b85354
Brayden Berg <berg31@tritons.iowacentral.edu>
2019-08-23 13:39:12 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,16 @@
+/*
+	Brayden Berg
+	2019
+	Due: August 29, 2019
+	Lab number 1, HelloWorld
+	HelloWorld is a project in which we are to follow directions to show our capability to download, copy, create, and submit a podject to and from GitHub.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "HelloWorld!";
+	std::cout << "My favorite color is red.";
+	return 0;
+}
